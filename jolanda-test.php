40a8de4e93b2ae33c596abf1a745797b3192123b
m93a <m93a.cz@gmail.com>
2013-12-09 20:51:14 +0100

    +jolanda chybové hlášky

---
+++ jolanda-test.php
@@ -0,0 +1,7 @@
+<?php
+$_JOLANDA['nadpis']="Spadlo to!";
+$_JOLANDA['zprava']="To není normální";
+include("view/begin.php");
+include("view/middle.php");
+include("view/jolanda.php");
+include("view/end.php");
